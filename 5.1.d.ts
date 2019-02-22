1a2fae02f9049c139457babc9fde984eed503f76
Endel Dreyer <endel.dreyer@gmail.com>
2019-02-22 16:28:57 -0300

    add math functions on 5.3-pre

--- 5.1.d.ts
+++ 5.1.d.ts
@@ -1,2 +1,4 @@
 import './core';
 import './special/5.1-only';
+import './special/5.3-pre';
+
--- 5.2.d.ts
+++ 5.2.d.ts
@@ -1,3 +1,4 @@
 import './core';
 import './special/5.2-plus';
 import './special/5.2-only';
+import './special/5.3-pre';
