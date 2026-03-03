556158ceb4639ea79bae9156933c57503ad655c0
Perry van Wesel <Perryvw@users.noreply.github.com>
2026-03-03 21:17:25 +0100

    Types for Lua 5.5 (#87)
    * Types for Lua 5.5
    
    * fix prettier

--- 5.4.d.ts
+++ 5.4.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="./core/index.d.ts" />
 /// <reference path="./special/5.4.d.ts" />
+/// <reference path="./version-specific-functions/utf8-decode/utf8-offset-5.3-5.4.d.ts" />
---
+++ 5.5.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="./core/index.d.ts" />
+/// <reference path="./special/5.5.d.ts" />
+/// <reference path="./version-specific-functions/utf8-decode/utf8-offset-5.5.d.ts" />
